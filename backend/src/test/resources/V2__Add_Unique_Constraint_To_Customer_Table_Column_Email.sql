7613783bc5c7cb70a5f2eacd79eafd4b0d543b0f
sasikumar6795 <32613154+sasikumar6795@users.noreply.github.com>
2023-05-13 10:15:00 +0530

    Testing workflow - query updated for constraint

--- backend/src/test/resources/V2__Add_Unique_Constraint_To_Customer_Table_Column_Email.sql
+++ backend/src/test/resources/V2__Add_Unique_Constraint_To_Customer_Table_Column_Email.sql
@@ -1,1 +1,2 @@
-ALTER TABLE customer ADD CONSTRAINT customer_email_unique UNIQUE (email);
+ALTER TABLE customer DROP CONSTRAINT  IF EXISTS customer_email_unique;
+ALTER TABLE customer ADD CONSTRAINT customer_email_unique UNIQUE (email) ;
